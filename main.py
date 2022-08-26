9e025f3c20239f5ebb62e15dfcc41bdf108fb7dc
gangi reddy <nallagondu@hotmail.com>
2022-08-26 17:49:21 +0000

    new how to push new datafileds in existing database

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 print("Hello ,World ! ")
 print("Hello this is my name:" + input("Name here"))
 print("Hello this is my Second name:" +  input("Second Name here"))
@@ -5,3 +6,25 @@ x = int(input("x value :"))
 y = x *2
 z = y * 20
 print(z)
+
+  
+# Define a dictionary containing Students data
+data = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+        'Height': [5.1, 6.2, 5.1, 5.2],
+        'Qualification': ['Msc', 'MA', 'Msc', 'Msc'],
+        'Contacts': [90303888,90303887,2387848,0]
+       }
+  
+# Convert the dictionary into DataFrame
+df = pd.DataFrame(data)
+  
+# Declare a list that is to be converted into a column
+address = ['Delhi', 'Bangalore', 'Chennai', 'Patna']
+weight = [67.3, 67.9,89.5,80]
+  
+# Using 'Address' as the column name
+# and equating it to the list
+df['Address'] = address 
+df['Weight'] = weight  
+# Observe the result
+print(df)
